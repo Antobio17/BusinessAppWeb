259f9b10e8d15539f01fe7dfe7335ad3dd3a1abc
Antonio Jiménez <antoniojr997@gmail.com>
2022-07-20 17:42:43 +0200

    (#9) Componente para pantalla de carga. Se ha creado el componente Loading para anclar pantallas de carga en las zonas deseadas. Se ha añadido maquetación less.

---
+++ src/common/components/Loading.js
@@ -0,0 +1,12 @@
+import './css/loading.scss';
+
+function Loading() {
+    return (
+        <div className="loading text-center">
+            <div className="loader"/>
+            <h3>CARGANDO</h3>
+        </div>
+    );
+}
+
+export default Loading;
