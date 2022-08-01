32392a684f8a068a73fec2c40cd8d37d57cfc78b
Antonio Jiménez <antoniojr997@gmail.com>
2022-08-01 18:49:52 +0200

    (#20) Funcionalidades de carrito de compra. Se ha añadido funcionalidad para el añadir o eliminar productos/cantidades del carrito de compra de la página Store. Se han pasado funcionalidades por props.

--- src/store/components/Product.js
+++ src/store/components/Product.js
@@ -1,17 +1,19 @@
 import React from "react";
 
+import {webServiceURL} from "../../App";
 import './css/product.scss';
 import '../../common/components/css/button.scss';
 
 function Product(props) {
+    const {amount, src, name, stock} = props.product;
 
     return (
         <article className="col-12 col-md-4 col-lg-4 product">
-            <img src={props.src} alt="Producto" width="230" height="200"/>
-            <p><span className="name">{props.name}</span></p>
-            <p><span className="amount">{props.amount}€</span></p>
-            {props.stock > 0 ?
-                <button className="btn btn-add-to-cart custom-btn">
+            <img src={webServiceURL + '/images/' + src} alt="Producto" width="230" height="200"/>
+            <p><span className="name">{name}</span></p>
+            <p><span className="amount">{amount}€</span></p>
+            {stock > 0 ?
+                <button className="btn btn-add-to-cart custom-btn" onClick={() => {props.addToCart(props.product)}}>
                     Añadir al carrito
                 </button> :
                 <p className="out-of-stock text-muted">SIN STOCK</p>
