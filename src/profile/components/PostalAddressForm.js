b31f7d44942b9a179558d4bcb61f6d88e813dd39
Antonio Jiménez <antoniojr997@gmail.com>
2022-08-14 18:41:31 +0200

    (#37) Trabajando en el formulario de direcciones postales.

---
+++ src/profile/components/PostalAddressForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './css/postal-address-form.scss';
+
+function PostalAddressForm(props) {
+    return (
+        <form onSubmit={(e) => {props.onSubmit(e)}} className="postal-address-form row">
+            <div className="col-12 col-md-6">
+                <input type="text" name="name" placeholder="Nombre..." value={props.name}
+                       onChange={(e) => props.setName(e.target.value)} required/>
+                <input type="text" name="address" placeholder="Dirección..." value={props.address}
+                       onChange={(e) => props.setAddress(e.target.value)} required/>
+                <input type="text" name="neighborhood" placeholder="Barrio... (Opcional)" value={props.neighborhood}
+                       onChange={(e) => props.setNeighborhood(e.target.value)}/>
+                <input type="number" name="name" placeholder="Código Postal..." value={props.postalCode}
+                       onChange={(e) => props.setPostalCode(e.target.value)}/>
+            </div>
+            <div className="col-12 col-md-6">
+                <input type="text" name="population" placeholder="Población..." value={props.population}
+                       onChange={(e) => props.setPopulation(e.target.value)}/>
+                <input type="text" name="province" placeholder="Provincia..." value={props.province}
+                       onChange={(e) => props.setProvince(e.target.value)}/>
+                <input type="text" name="state" placeholder="País..." value={props.state}
+                       onChange={(e) => props.setState(e.target.value)}/>
+                <input className="btn btn-profile custom-btn" type="submit" name="submit" value={props.btnLabel}/>
+            </div>
+        </form>
+    );
+}
+
+export default PostalAddressForm;
