079e5a28a218d45afec27dc980c5b378a6f68aeb
Antonio Jiménez <antoniojr997@gmail.com>
2022-08-16 22:45:03 +0200

    (#37, #38) Trabajando en el panel de direcciones postales. Se ha añadido la funcionalidad para actualizar o crear una dirección postal en el Web Service. Se ha añadido nuevos estados.

--- src/profile/components/PostalAddressForm.js
+++ src/profile/components/PostalAddressForm.js
@@ -4,7 +4,9 @@ import './css/postal-address-form.scss';
 
 function PostalAddressForm(props) {
     return (
-        <form onSubmit={(e) => {props.onSubmit(e)}} className="postal-address-form row">
+        <form onSubmit={(e) => {
+            props.onSubmit(e)
+        }} className="postal-address-form row">
             <div className="col-12 col-md-6">
                 <input type="text" name="name" placeholder="Nombre..." value={props.name}
                        onChange={(e) => props.setName(e.target.value)} required/>
@@ -13,15 +15,16 @@ function PostalAddressForm(props) {
                 <input type="text" name="neighborhood" placeholder="Barrio... (Opcional)" value={props.neighborhood}
                        onChange={(e) => props.setNeighborhood(e.target.value)}/>
                 <input type="number" name="name" placeholder="Código Postal..." value={props.postalCode}
-                       onChange={(e) => props.setPostalCode(e.target.value)}/>
+                       onChange={(e) => props.setPostalCode(e.target.value)} required/>
             </div>
             <div className="col-12 col-md-6">
                 <input type="text" name="population" placeholder="Población..." value={props.population}
-                       onChange={(e) => props.setPopulation(e.target.value)}/>
+                       onChange={(e) => props.setPopulation(e.target.value)} required/>
                 <input type="text" name="province" placeholder="Provincia..." value={props.province}
-                       onChange={(e) => props.setProvince(e.target.value)}/>
+                       onChange={(e) => props.setProvince(e.target.value)} required/>
                 <input type="text" name="state" placeholder="País..." value={props.state}
-                       onChange={(e) => props.setState(e.target.value)}/>
+                       onChange={(e) => props.setState(e.target.value)} required/>
+                {props.selectedID !== undefined && <input type="hidden" name="id" value={props.selectedID}/>}
                 <input className="btn btn-profile custom-btn" type="submit" name="submit" value={props.btnLabel}/>
             </div>
         </form>
