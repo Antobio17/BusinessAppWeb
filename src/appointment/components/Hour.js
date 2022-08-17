226f3fff356ba3f2abe9d917c5615224368e160b
Antonio Jiménez <antoniojr997@gmail.com>
2022-08-17 19:05:37 +0200

    :hammer:

--- src/appointment/components/Hour.js
+++ src/appointment/components/Hour.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './css/hour.scss';
 
 import {bookUserAppointment} from '../../services/appointment';
 
 function Hour(props) {
+    const [booking, setBooking] = useState(false);
+
     const bookAppointment = () => {
         const day = props.day;
         const splitHour = props.hour.split(':');
         const bookingDateAt = day.setHours(splitHour[0], splitHour[1], splitHour[2]) / 1000;
 
+        setBooking(true);
         Promise.all([
             bookUserAppointment(bookingDateAt)
         ]).then(response => {
@@ -18,18 +21,25 @@ function Hour(props) {
             if (data.result) {
                 props.onBookingMade();
             }
+            setBooking(false);
         }).catch(error => {
             console.log('Error al recuperar la configuración del horario del negocio: ' + error);
+            setBooking(false);
         });
     };
 
     return (
         <article className="hour">
             <p className="col-6 m-auto fw-bold">{props.hour}</p>
-            {props.available && !props.hasPendingAppointment ?
-                <button className="btn btn-appointment custom-btn col-6 fw-bold" onClick={() => bookAppointment()}>
-                    Reservar
-                </button> :
+            {props.available && !props.hasPendingAppointment ? (
+                    booking ?
+                        <div className="login-loader">
+                            <div className="lds-dual-ring"/>
+                        </div> :
+                        <button className="btn btn-appointment custom-btn col-6 fw-bold" onClick={() => bookAppointment()}>
+                            Reservar
+                        </button>
+                ) :
                 <button className="col-6 btn btn-appointment custom-btn btn-appointment-disabled fw-bold" disabled>
                     {props.hasPendingAppointment ? 'Reserva Activa' : 'No disponible'}
                 </button>
